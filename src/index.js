fe3c910e821a1a1c8e62f1d33d44bb7fcd42a424
hanxiaojian <1095906242@qq.com>
2022-09-30 20:14:00 +0800

    JSX阶段总结

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import ReactDOM from "react-dom";
+
+import "./CSS/index.css";
+// import { createRoot } from 'react-dom/client'
+
+// const title = React.createElement('h1', null, 'Hello world !!!');
+
+// ReactDOM.render(title, document.getElementById('root'));
+// // const container = document.getElementById('root');
+// // const root = createRoot(container);
+// // root.render(title);
+// // react-dom.development.js:86 Warning: ReactDOM.render is no longer supported in React 18.
+// // Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17.
+// // Learn more: https://reactjs.org/link/switch-to-createroot
+
+const songs = [
+  { id: 1, name: "chixjd" },
+  { id: 2, name: "xwzydr" },
+  { id: 3, name: "nsn" },
+];
+const list = (
+  <ul style={{ color: "blue" }} className="ccc">
+    {songs.map((item) => (
+      <li key={item.id}>{item.name}</li>
+    ))}
+  </ul>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 渲染react元素
+ReactDOM.render(list, document.getElementById("root"));
