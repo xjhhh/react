2e24ef6739dec5e1243c8f15daf03a2d3b7466fd
hanxiaojian <1095906242@qq.com>
2022-10-16 23:51:45 +0800

    模糊匹配

--- src/code/index.26默认路由.js
+++ src/code/index.26默认路由.js
@@ -10,11 +10,16 @@ import Tools from "./tools";
 // 优化 2.清理
 
 const Home = () => <p>你能看到我吗？</p>;
+const Login = () => <p>我是login组件的内容</p>;
 const App = () => (
   <Router>
     <div>
-      <h1>默认路由</h1>
+      <h1>默认路由和模糊匹配</h1>
+      {/* 模糊匹配：只要to属性以某个path开头，就可以匹配这个path */}
+      <Link to="/login">登录页面</Link>
+      {/* 默认路由 */}
       <Route path="/" component={Home}></Route>
+      <Route path="/login" component={Login}></Route>
     </div>
   </Router>
 );
