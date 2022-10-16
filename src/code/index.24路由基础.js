faee3aaa0b6b9029c3f37101bd5f0757ab5a804a
hanxiaojian <1095906242@qq.com>
2022-10-16 22:14:43 +0800

    编程式导航

--- src/code/index.24路由基础.js
+++ src/code/index.24路由基础.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import cat from "./images/cat.png";
 import PropTypes from "prop-types";
@@ -9,8 +9,8 @@ import Tools from "./tools";
 // 优化 1.属性校验
 // 优化 2.清理
 
-const First = () => <p>页面1的内容</p>
-const Home = () => <h3>主页的内容</h3>
+const First = () => <p>页面1的内容</p>;
+const Home = () => <h3>主页的内容</h3>;
 
 const App = () => (
   // Router包裹整个应用
@@ -22,13 +22,11 @@ const App = () => (
       <br></br>
       <Link to="/home">主页</Link>
       {/* 指定路由出口 */}
-      <Routes>
-        <Route path="/first" element={<Home/>} />
-        <Route path="/home" element={<Home/>} />
-        {/*   第二个first不会被匹配到 */}
-        <Route path="/first" element={<First/>} />
-      </Routes>
+      <Route path="/first" component={Home} />
+      <Route path="/home" component={Home} />
+      {/*   第二个first不会被匹配到 */}
+      <Route path="/first" component={First} />
     </div>
   </Router>
-)
+);
 ReactDOM.render(<App> </App>, document.getElementById("root"));
