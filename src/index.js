21c1f7183207013e3055600e87104aade3f26c91
hanxiaojian <1095906242@qq.com>
2022-10-16 15:07:37 +0800

    后面的同名路径不会被匹配

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import Tools from "./tools";
 // 优化 2.清理
 
 const First = () => <p>页面1的内容</p>
+const Home = () => <h3>主页的内容</h3>
 
 const App = () => (
   // Router包裹整个应用
@@ -18,9 +19,14 @@ const App = () => (
       <h1>React 路由基础</h1>
       {/* 指定路由入口 */}
       <Link to="/first">页面1</Link>
+      <br></br>
+      <Link to="/home">主页</Link>
       {/* 指定路由出口 */}
       <Routes>
-        <Route path="/first" element={<First></First>} />
+        <Route path="/first" element={<Home/>} />
+        <Route path="/home" element={<Home/>} />
+        {/*   第二个first不会被匹配到 */}
+        <Route path="/first" element={<First/>} />
       </Routes>
     </div>
   </Router>
